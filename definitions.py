89a1c6afc8436f9afdd8a3235cbec28ef92c3ca5
Krister S Jakobsson <krister.s.jakobsson@gmail.com>
2019-03-12 16:57:57 +0900

    Deleted unnecessary defined variable

--- definitions.py
+++ definitions.py
@@ -3,7 +3,6 @@ from os.path import dirname, abspath, join
 ROOT_DIR = join(dirname(abspath(__file__)))  # This is the path to the root
 SOURCE_DIR = join(ROOT_DIR, "src")
 
-CLASS_PATH = join(ROOT_DIR, "classes")
 RESOURCES_PATH = join(ROOT_DIR, "resources")
 OUTPUT_PATH = join(ROOT_DIR, "output")
 CLASSIFIERS_PATH = join(OUTPUT_PATH, "classifiers")
